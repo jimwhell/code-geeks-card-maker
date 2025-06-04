73287b8acd24ac3369420ab2f984f17ebc4f3878
jimwell <jimwelllmanalo@gmail.com>
2025-06-04 18:39:13 +0800

    (feat): Add spinner logic

--- client/src/app/components/login-form/login-form.component.ts
+++ client/src/app/components/login-form/login-form.component.ts
@@ -14,6 +14,7 @@ import { Router } from '@angular/router';
 export class LoginFormComponent {
   isSubmitted: boolean = false;
   serverError: string | null = null;
+  isLoading: boolean = false;
 
   private formBuilder = inject(FormBuilder);
 
@@ -41,26 +42,30 @@ export class LoginFormComponent {
   }
 
   onSubmit(): void {
+    console.log('Called');
     this.isSubmitted = true;
+    this.isLoading = true;
     if (this.loginForm.valid) {
       const email: string = this.loginForm.value.email as string;
       const password: string = this.loginForm.value.password as string;
 
-      this.loginForm.reset();
       this.loginForm.markAsPristine();
       this.loginForm.markAsUntouched();
+      this.loginForm.reset();
 
       this.authService.login(email, password).subscribe({
         next: (response) => {
+          this.isLoading = false;
           console.log(response);
           this.isSubmitted = false;
           this.router.navigate(['/dashboard']);
         },
         error: (err) => {
+          this.isLoading = false;
           this.isSubmitted = false;
           console.error('Error in logging in admin: ', err);
           if (err.status === 401) {
-            this.serverError = 'Invalid email or password';
+            this.serverError = 'Invalid email or password.';
             return;
           }
           this.serverError = 'Something went wrong. Please try again.';
@@ -68,6 +73,6 @@ export class LoginFormComponent {
       });
       return;
     }
-    this.isSubmitted = false;
+    this.isLoading = false;
   }
 }
