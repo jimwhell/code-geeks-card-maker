c30784b7ef53bdceac1f74279e221dd44f31ae97
jimwell <jimwelllmanalo@gmail.com>
2025-06-04 15:23:53 +0800

    (feat): Create refresh token handler

--- server/src/controllers/sessions.controller.ts
+++ server/src/controllers/sessions.controller.ts
@@ -2,16 +2,23 @@ import { NextFunction, Request, Response } from "express";
 import {
   createSession,
   findSessions,
+  reIssueAccessToken,
   updateSession,
   validatePassword,
 } from "../models/session.model";
 import { signJWT } from "../utils/jwt.utils";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
-import { admin } from "googleapis/build/src/apis/admin";
-import { Session } from "../types/session.types";
+import {
+  AccessTokenResponse,
+  LogoutTokenResponse,
+  Session,
+} from "../types/session.types";
 import log from "../utils/logger";
 import { loginAdminInput } from "../schemas/session.schema";
+import { convertToSeconds } from "../utils/timeConverter";
+import { findAdminByID } from "../models/admin.model";
+import { Admin } from "../types/admin.types";
 
 dotenv.config();
 
@@ -20,7 +27,7 @@ const refreshTokenTTL = process.env.REFRESH_TOKEN_TTL;
 
 export async function createAdminSession(
   req: Request<{}, {}, loginAdminInput["body"]>,
-  res: Response,
+  res: Response<AccessTokenResponse>,
   next: NextFunction
 ) {
   //validate input credentials
@@ -28,22 +35,28 @@ export async function createAdminSession(
     const admin = await validatePassword(req.body);
 
     if (!admin) {
-      res.status(401).json({ message: "Invalid email or password" });
-      return;
+      throw new Error("Admin ID not found");
     }
 
     //create a session
     const adminId = admin.admin_id;
 
     if (!adminId) {
-      res.status(404).json({ message: "Admin id not found." });
-      return;
+      throw new Error("Admin ID not found");
     }
 
     const session: Session | null = await createSession(adminId);
 
     if (session === null) {
       throw new Error("Failed to create session");
+    }
+
+    if (!accessTokenTTL) {
+      throw new Error("Access token TTL not found");
+    }
+
+    if (!refreshTokenTTL) {
+      throw new Error("Refresh token TTL not found");
     }
 
     //create an access token
@@ -68,7 +81,20 @@ export async function createAdminSession(
       throw new Error("Failed to create refresh token");
     }
 
-    res.send({ accessToken, refreshToken });
+    const accessTokenExpiresAt: number = convertToSeconds(accessTokenTTL);
+    const refreshTokenExpiresAt: number = convertToSeconds(refreshTokenTTL);
+
+    res.cookie("refresh_token", refreshToken, {
+      path: "/api/sessions/refresh",
+      httpOnly: true,
+      maxAge: 365 * 24 * 60 * 60 * 1000,
+      sameSite: "lax",
+    });
+
+    res.status(200).json({
+      token: accessToken,
+      expiresIn: accessTokenExpiresAt,
+    });
   } catch (error) {
     log.error("Server error in creating a new session");
     next(error);
@@ -84,15 +110,13 @@ export async function getAdminSessions(
     const adminId: string = res.locals.admin.admin_id;
 
     if (!adminId) {
-      res.status(400).json({ message: "Admin ID not found" });
-      return;
+      throw new Error("Admin ID not found");
     }
 
     const sessions: Session[] | null = await findSessions(adminId);
 
     if (sessions === null) {
-      res.status(404).send("No sessions found for admin.");
-      return;
+      throw new Error("No sessions found for admin");
     }
     res.status(200).json(sessions);
   } catch (error) {
@@ -102,15 +126,14 @@ export async function getAdminSessions(
 
 export async function deleteAdminSession(
   req: Request,
-  res: Response,
+  res: Response<LogoutTokenResponse>,
   next: NextFunction
 ) {
   try {
     const sessionId: string = res.locals.admin.session;
 
     if (!sessionId) {
-      res.status(400).json({ message: "Missing session ID." });
-      return;
+      throw new Error("Session id not found");
     }
 
     //invalidate the admin's current session
@@ -128,6 +151,37 @@ export async function deleteAdminSession(
       refreshToken: null,
     });
   } catch (error) {
+    next(error);
+  }
+}
+
+export async function refreshAccessToken(
+  req: Request,
+  res: Response<AccessTokenResponse>,
+  next: NextFunction
+) {
+  try {
+    const refreshToken: string = req.cookies["refresh_token"];
+    console.log("Req cookies: ", req.cookies);
+
+    if (!refreshToken) {
+      throw new Error("Refresh token not found");
+    }
+
+    const accessToken = await reIssueAccessToken({
+      refreshToken,
+    });
+
+    if (!accessToken || accessToken === null) {
+      throw new Error("Access token not found");
+    }
+
+    res.status(200).json({
+      token: accessToken.token,
+      expiresIn: accessToken.expiresIn,
+    });
+  } catch (error) {
+    log.error("Server error: ", error);
     next(error);
   }
 }
