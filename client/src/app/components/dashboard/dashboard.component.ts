894d586489e6a588a481235474c2f53fb78692bf
jimwell <jimwelllmanalo@gmail.com>
2025-06-04 22:05:52 +0800

    (chore): Define logic for mobile navbar visibility

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  protected menuState: 'open' | 'closed' = 'closed';
+  toggleMenuState(): void {
+    this.menuState = this.menuState === 'closed' ? 'open' : 'closed';
+  }
+
+  toggleMenuStateFromChild(state: 'open' | 'closed'): void {
+    this.menuState = state;
+  }
+}
